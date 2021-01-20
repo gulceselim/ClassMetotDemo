1a9f4e32ce65387f1637574926137b0aa0d772b9
gulceselim <selimgulce@hotmail.com>
2021-01-20 21:07:50 +0300

    refactored project

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class Musteri
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string TelefonNo { get; set; }
+
+        public Musteri(int ıd, string ad, string soyad, string telefonNo)
+        {
+            Id = ıd;
+            Ad = ad;
+            Soyad = soyad;
+            TelefonNo = telefonNo;
+        }
+    }
+}
---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        Musteri[] musteriler;
+        public void AddMusteri(params Musteri[] musteri)
+        {
+            musteriler = musteri;
+            Console.WriteLine($"{musteri.Length} Müşteri veritabanına eklendi.");
+        }
+
+        public void DeleteMusteri(Musteri musteri)
+        {
+            foreach (var m in musteriler)
+            {
+                if (m.Id == musteri.Id)
+                {
+                    List<Musteri> tmp = new List<Musteri>(musteriler);
+                    tmp.Remove(m);
+                    musteriler = tmp.ToArray();
+                    break;
+                }
+            }
+            Console.WriteLine($"{musteri.Ad} {musteri.Soyad} adlı müşteri veritabanından silindi.");
+        }
+
+        public void GetMusteri()
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine($"ID: {musteri.Id}\n" +
+                                    $"İsim: {musteri.Ad}\n" +
+                                    $"Soyisim: {musteri.Soyad}\n" +
+                                    $"Telefon Numarası: {musteri.TelefonNo}\n" +
+                                    $"------------------------------");
+            }
+
+
+
+        }
+    }
+}
